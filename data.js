21b2fd2ee604206385d184c1d05ac7b9ab09e2a0
Antoniogordilloe <aegordillo2@gmail.com>
2023-11-22 11:31:44 +0100

    Translates DAy, Month, City

---
+++ data.js
@@ -0,0 +1,20 @@
+const months = [
+    "January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"
+  ];
+  const cities = [
+    "Madrid", "Barcelona", "Seville", "Zaragoza", "Valencia", "Málaga",
+    "Murcia", "Palma", "Las Palmas de Gran Canaria", "Bilbao", "Alicante",
+    "Córdoba", "Valladolid", "Vigo", "Gijón", "L'Hospitalet de Llobregat",
+    "Vitoria-Gasteiz", "Santa Cruz de Tenerife", "Santander", "Badalona",
+    "Cartagena", "Terrassa", "Jerez de la Frontera", "Sabadell",
+    "Alcalá de Henares", "Fuenlabrada", "Pamplona", "Almería", "San Sebastián",
+    "Castellón de la Plana", "Burgos", "Santander", "Albacete", "Alcorcón",
+    "Getafe", "Salamanca", "Huelva", "Logroño", "Badajoz", "Tarragona", "Lleida",
+    "Ourense", "Algeciras", "León", "Alcobendas", "Cádiz", "Jaén", "Oviedo",
+    "Móstoles", "Elche", "Sabadell", "Jerez de la Frontera", "Terrassa",
+    "Sabadell", "Girona", "Cáceres", "Lugo", "Pontevedra", "Toledo", "Segovia",
+    "Ávila", "Cuenca", "Huesca", "Palencia", "Zamora"
+  ];
+
+  export { months, cities };
---
+++ index.js
@@ -0,0 +1,102 @@
+
+ import { months, cities } from './data.js';
+  
+
+
+
+ var parameter = process.argv.slice(2).join(' ');
+
+// var parameter = "12 1 Madri";
+
+
+  var index = 0;
+  
+  var day = 0;
+  var month = "";
+  var city = "";
+  
+  processString(parameter);
+  
+  function processString(parameter) {
+    var dayResult = determineDay(parameter);
+    dayResult ? day = dayResult.day : day = 0;
+  
+    var monthResult = determineMonth(parameter);
+    monthResult ? month = monthResult : month = "";
+  
+    var cityResult = determineCity(parameter);
+    cityResult ? city = cityResult : city = "";
+  
+    console.log("Day: " + day);
+    console.log("Month: " + month);
+    console.log("City: " + city);
+  }
+  
+  function determineDay(parameter) {
+    var dayMatch = parameter.match(/\d+/);
+  
+    if (dayMatch) {
+      var day = dayMatch[0];
+      console.log("Day found");
+  
+      // Find the index where the day was found
+      var index = parameter.indexOf(day) + 1;
+  
+      // Return an object containing both the day and its index
+      return { day: day, index: index };
+    } else {
+      console.log("No day found");
+      return null;
+    }
+  }
+  
+  function determineMonth(parameter) {
+
+    for (var i = index; i < months.length; i++) {
+      var month = months[i];
+      var result = matchMost(parameter, month, 3);
+      if (result) {
+        console.log("Month found");
+        return result;
+      }
+    }
+
+    var monthNumber = parseInt(parameter);
+    
+    if (!isNaN(monthNumber) ) {
+      var month = months[parameter];
+      console.log("Month found");
+      return months[monthNumber-1];
+    }
+  
+    console.log("No month found");
+    return null;
+  }
+  
+  function determineCity(parameter) {
+    for (var i = 0; i < cities.length; i++) {
+      var city = cities[i];
+      var result = matchMost(parameter, city, 4);
+      if (result) {
+        console.log("City found");
+        return result;
+      }
+    }
+  
+    console.log("No city found");
+    return null;
+  }
+  
+  function matchMost(find, string, accuracy) {
+    for (var i = 0; i < find.length; i++) {
+      for (var j = find.length; j > i; j--) {
+        var substring = find.substring(i, j);
+        if (substring.length >= accuracy && string.indexOf(substring) !== -1) {
+          return string;  // Return the entire word
+        }
+      }
+    }
+  
+    return null;
+  }
+  
