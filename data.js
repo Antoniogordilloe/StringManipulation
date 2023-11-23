31ad0435fb80a1466a4ad39832c08f0212613a1b
Antoniogordilloe <aegordillo2@gmail.com>
2023-11-23 10:52:35 +0100

    Dynamic Accuracy

--- data.js
+++ data.js
@@ -1,28 +1,26 @@
-import fs from 'fs';
+import fs from 'fs'
 
 const months = [
-    "January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"
-  ];
+  'January', 'February', 'March', 'April', 'May', 'June', 'July',
+  'August', 'September', 'October', 'November', 'December'
+]
 
-  function createArraysFromJSON(jsonData) {
-    const countries = {};
-  
-    for (const country in jsonData) {
-      if (jsonData.hasOwnProperty(country)) {
-        countries[country] = jsonData[country];
-      }
+function createArraysFromJSON (jsonData) {
+  const countries = {}
+
+  for (const country in jsonData) {
+    if (jsonData.hasOwnProperty(country)) {
+      countries[country] = jsonData[country]
     }
-  
-    return countries;
   }
-  
-  // Read countries.json file
-  const rawData = fs.readFileSync('countries.json');
-  const jsonData = JSON.parse(rawData.toString());
-  
-  const countriesArray = createArraysFromJSON(jsonData);
-  
 
- 
-  export { months,  countriesArray };
+  return countries
+}
+
+// Read countries.json file
+const rawData = fs.readFileSync('countries.json')
+const jsonData = JSON.parse(rawData.toString())
+
+const countriesArray = createArraysFromJSON(jsonData)
+
+export { months, countriesArray }
--- index.js
+++ index.js
@@ -1,104 +1,98 @@
+import { months, countriesArray } from './data.js'
 
- import { months, countriesArray } from './data.js';
+const cities = countriesArray.Spain
 
- var cities = countriesArray["Spain"];
+const parameter = process.argv.slice(2).join(' ')
 
- var parameter = process.argv.slice(2).join(' ');
+const index = 0
 
-// var parameter = "12 1 Madri";
+let day = 0
+let month = ''
+let city = ''
 
+processString(parameter)
 
-  var index = 0;
-  
-  var day = 0;
-  var month = "";
-  var city = "";
-  
-  processString(parameter);
-  
-  function processString(parameter) {
-    var dayResult = determineDay(parameter);
-    dayResult ? day = dayResult.day : day = 0;
-  
-    var monthResult = determineMonth(parameter);
-    monthResult ? month = monthResult : month = "";
-  
-    var cityResult = determineCity(parameter);
-    cityResult ? city = cityResult : city = "";
-  
-    console.log("Day: " + day);
-    console.log("Month: " + month);
-    console.log("City: " + city);
+function processString (parameter) {
+  const dayResult = determineDay(parameter)
+  dayResult ? day = dayResult.day : day = 0
+
+  const monthResult = determineMonth(parameter)
+  monthResult ? month = monthResult : month = ''
+
+  const cityResult = determineCity(parameter)
+  cityResult ? city = cityResult : city = ''
+
+  console.log('Day: ' + day)
+  console.log('Month: ' + month)
+  console.log('City: ' + city)
+}
+
+function determineDay (parameter) {
+  const dayMatch = parameter.match(/\d+/)
+
+  if (dayMatch) {
+    const day = dayMatch[0]
+    console.log('Day found')
+
+    const index = parameter.indexOf(day) + 1
+
+    return { day, index }
+  } else {
+    console.log('No day found')
+    return null
   }
-  
-  function determineDay(parameter) {
-    var dayMatch = parameter.match(/\d+/);
-  
-    if (dayMatch) {
-      var day = dayMatch[0];
-      console.log("Day found");
-  
-      // Find the index where the day was found
-      var index = parameter.indexOf(day) + 1;
-  
-      // Return an object containing both the day and its index
-      return { day: day, index: index };
-    } else {
-      console.log("No day found");
-      return null;
+}
+
+function determineMonth (parameter) {
+  for (let i = index; i < months.length; i++) {
+    var month = months[i]
+    const result = matchMost(parameter, month, 3)
+    if (result) {
+      console.log('Month found')
+      return result
     }
   }
-  
-  function determineMonth(parameter) {
 
-    for (var i = index; i < months.length; i++) {
-      var month = months[i];
-      var result = matchMost(parameter, month, 3);
+  const monthNumber = parseInt(parameter)
+
+  if (!isNaN(monthNumber)) {
+    var month = months[parameter]
+    console.log('Month found')
+    return months[monthNumber - 1]
+  }
+
+  console.log('No month found')
+  return null
+}
+
+function determineCity (parameter) {
+  const maxAccuracy = 10
+
+  for (let accuracy = maxAccuracy; accuracy >= 0; accuracy--) {
+    for (let i = 0; i < cities.length; i++) {
+      const city = cities[i]
+
+      const result = matchMost(parameter, city, accuracy)
+
       if (result) {
-        console.log("Month found");
-        return result;
+        console.log('City found')
+        return result
       }
     }
+  }
+  console.log('No city found')
+  return null
+}
 
-    var monthNumber = parseInt(parameter);
-    
-    if (!isNaN(monthNumber) ) {
-      var month = months[parameter];
-      console.log("Month found");
-      return months[monthNumber-1];
-    }
-  
-    console.log("No month found");
-    return null;
-  }
-  
-  function determineCity(parameter) {
-    for (var i = 0; i < cities.length; i++) {
-      var city = cities[i];
-      var result = matchMost(parameter, city, 5);
-      if (result) {
-        console.log("City found");
-        return result;
+function matchMost (find, string, accuracy) {
+  for (let i = 0; i < find.length; i++) {
+    for (let j = find.length; j > i; j--) {
+      const substring = find.substring(i, j)
+      if (substring.length >= accuracy && string.indexOf(substring) !== -1) {
+        return string
       }
     }
-  
-    console.log("No city found");
-    return null;
-  }
-  
-  function matchMost(find, string, accuracy) {
-    
-    for (var i = 0; i < find.length; i++) {
-      for (var j = find.length; j > i; j--) {
-        var substring = find.substring(i, j);
-        if (substring.length >= accuracy && string.indexOf(substring) !== -1) {
-          return string;  // Return the entire word
-        }
-      }
-    }
-  
-    return null;
   }
 
- 
-  
+  return null
+}
